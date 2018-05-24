abc89cae905220eb986b408a6f8b12d787c5ee92
KDGIT <futurefbi07@aol.com>
2018-05-23 23:56:05 -0400

    Changes to Stars

--- js/app.js
+++ js/app.js
@@ -145,7 +145,7 @@ function cardMatch() {
 function endGame() {
   modal.style.display = "block";
   var h = document.createTextNode(
-    "You made " + (moves - 1) + " moves in " + timer.innerHTML + " and earned a star rating of " +  + " !"
+    "You made " + (moves - 1) + " moves in " + timer.innerHTML + " and earned a star rating of " + stars + " !"
   );
   content.appendChild(h);
   stopTimer();
